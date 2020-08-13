91278b41c95fc3a42a4436faa8276be1d35b8b37
Stoyan Manolov <stoyanmanolovvv@gmail.com>
2020-08-13 16:32:54 +0300

    Updates

---
+++ precache-manifest.9ca028a037532adc60b6d0fa7711a2e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da89c181196016101f9c49bb5c9bbb25",
+    "url": "/typespeed/index.html"
+  },
+  {
+    "revision": "5120289a2802522b59cb",
+    "url": "/typespeed/static/css/main.426bdc55.chunk.css"
+  },
+  {
+    "revision": "5aa2ebc9964552882187",
+    "url": "/typespeed/static/js/2.3ef6087c.chunk.js"
+  },
+  {
+    "revision": "5120289a2802522b59cb",
+    "url": "/typespeed/static/js/main.60552f9f.chunk.js"
+  },
+  {
+    "revision": "d119d03052fc805c218f",
+    "url": "/typespeed/static/js/runtime-main.5f70c8ff.js"
+  },
+  {
+    "revision": "2f1c35ecf8955ac131b42c3dec9672cb",
+    "url": "/typespeed/static/media/fireworks.2f1c35ec.gif"
+  }
+]);
