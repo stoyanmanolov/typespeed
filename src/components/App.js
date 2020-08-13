5433c507d744c812ccee700ecd4573143248e71c
Stoyan Manolov <stoyanmanolovvv@gmail.com>
2020-08-13 17:11:55 +0300

    Replacing BrowserRouter with HashRouter

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,17 @@ import React from "react";
 import Home from "./Home";
 import Race from "./Race";
 import Ending from "./Ending";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { HashRouter, Switch, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <HashRouter basename="/">
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/race" component={Race} />
         <Route path="/end" component={Ending} />
       </Switch>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
